b1683d63ad63b287af3f27f2b46e3579eebd9194
johnathanwastaken <danielortiz2001@hotmail.com>
2022-06-01 21:23:16 -0700

    Adding default view for layout

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
+const place = express.Router();
 
-router.get('/', (req, res) =>{
+// INDEX
+place.get('/', (req, res) =>{
     res.send('GET /places')
 })
+
+// NEW
+place.get("/new", (req, res) => {
+    res.render("new");
+})
+
 
 module.exports = router
